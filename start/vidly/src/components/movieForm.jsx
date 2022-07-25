5dfa7c0c6c1e04e92b8e3d549a3105f4c78b3b7d
HASNE HOSSAIN <hasne.hossain@student.unimelb.edu.au>
2022-07-26 01:48:43 +1000

    partial implementation of movie nd its form

--- start/vidly/src/components/movieForm.jsx
+++ start/vidly/src/components/movieForm.jsx
@@ -1,15 +1,71 @@
 import React from 'react';
+import MovieForm from './../../../../finish/vidly/src/components/movieForm';
+import Joi from 'joi-browser';
+import Form from './common/form';
 
-const movieForm = ({match, history}) => {
+//const movieForm = ({match, history}) => {
+//const movieForm = (props) => {
+class MovieForm extends Form{
   //const {match, history} = this.props; //don't know why "match" was not getting extracted like this...LEARN
-  return (
+
+  state = {
+    data: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: ""
+    },
+    genres: [],
+    errors: {}
+  };
+
+  schema = {
+    _id: Joi.string(), //"required" not added coz there could be 0 movies
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    genreId: Joi.string()
+      .required()
+      .label("Genre"),
+    numberInStock: Joi.number()
+      .required()
+      .min(0)
+      .max(100)
+      .label("Number in Stock"),
+    dailyRentalRate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Daily Rental Rate")
+  };
+
+
+  componentDidMount() {
+    const genres = getGenres();
+    this.setState({ genres });
+
+    const movieId = this.props.match.params.id; //the id comes from the movie clicked
+                                        //in movies.jsx
+    if (movieId === "new") return;
+
+    const movie = getMovie(movieId);
+    if (!movie) return this.props.history.replace("/not-found");
+
+    this.setState({ data: this.mapToViewModel(movie) });
+  }
+
+
+  //return (
+  render(){
+    return(
     <div>
-        <h1>movieForm {match.params.id}</h1>
+        <h1>movieForm {this.props.match.params.id}</h1>
         <button className='btn btn-primary' onClick={() => history.push("/movies")}>
           Save
         </button>
     </div>
-  )
+    )
+  }
 }
 
 export default movieForm
